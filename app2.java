0a4eb8e94058ee6efb1494bc84e9ddbbb77d06b6
lovelySo03 <143456833+lovelySo03@users.noreply.github.com>
2023-10-12 12:15:01 +0900

    Add files via upload

---
+++ app2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class app2 {
+ public static void main(String[] args) {
+
+        Double a;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("부가세를 제외한 상품의 금액을 입력하세요");
+        a = sc.nextDouble();
+
+        double valueOfSupply = a;
+        double vatRate = 0.1;
+        double expenseRate = 0.3;
+        double vat = valueOfSupply * vatRate;
+        double total = valueOfSupply + vat;
+        double expense = valueOfSupply * expenseRate;
+        double income = valueOfSupply - expense;
+        double dividend1 = income * 0.5;
+        double dividend2 = income * 0.3;
+        double dividend3 = income * 0.2;
+ 
+        System.out.println("Value of supply : " + valueOfSupply);
+        System.out.println("VAT : " + vat);
+        System.out.println("Total : " + total);
+        System.out.println("Expense : " + expense);
+        System.out.println("Income : " + income);
+        System.out.println("Dividend 1 : " + dividend1);
+        System.out.println("Dividend 2 : " + dividend2);
+        System.out.println("Dividend 3 : " + dividend3);
+ 
+    }
+ 
+}
